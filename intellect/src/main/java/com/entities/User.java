0957d5b60eb4c91d6985e4b7755a8a0ab3cbf505
Nitin Kumar <nitin.kumar1701@gmail.com>
2017-11-24 20:45:25 +0530

    Add files via upload

---
+++ intellect/src/main/java/com/entities/User.java
@@ -0,0 +1,86 @@
+package com.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private String 		id;
+
+	@Column(name = "First_Name", updatable = false)
+	private String 		fName;
+	
+	@Column(name = "Last_Name", updatable = false)
+	private String 		lName;
+	
+	@Column(name = "Email", updatable = false)
+	private String 		email;
+	
+	@Column(name = "PinCode", updatable = true)
+	private Integer 	pinCode;
+	
+	@Column(name = "Birth_Date", updatable = true)
+	private Date 		birthDate;
+	
+	@Column(name = "Is_Active", updatable = true)
+	private Boolean 	isActive;
+
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(Integer pinCode) {
+		this.pinCode = pinCode;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
